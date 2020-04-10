d3a4dd6ab140a9d5bab1ff4cb321baeec6100f15
Tikhoplav <TikhoplavSPB@gmail.com>
2020-04-10 17:23:56 +0300

    added content-length header and json support

--- main.go
+++ main.go
@@ -10,18 +10,18 @@ import (
 
 func main() {
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		var body []byte
 
-		html := make([]string, 0, 1024)
-		html = append(html, "<html><body>")
-		html = append(html, "<h3>Swarm net test:</h3>")
-		html = append(html, fmt.Sprintf("<p>Current container time: %v<br>", time.Now()))
-		html = append(html, "<i>(if not changed after reload, then this page is cached)</i></p>")
-		html = append(html, fmt.Sprintf("<p>Container id: <b>%v</b><br>", DockerId()))
-		html = append(html, "<i>(this id is the same as in `docker ps`)</i></p>")
-		html = append(html, "</body></html>")
+		if accept, ok := r.Header["Accept"]; ok && strings.Contains(accept[0], "text/html") {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			body = CreateHTML()
+		} else {
+			w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			body = CreateJSON()
+		}
 
-		w.Write([]byte(strings.Join(html, "\n")))
+		w.Header().Set("Content-Length", fmt.Sprintf("%v", len(body)))
+		w.Write(body)
 	})
 
 	fmt.Println("Starting server at 8080")
@@ -37,5 +37,26 @@ func DockerId() string {
 	s := fmt.Sprintf("%s", cont)
 	ss := strings.Split(s, "/")
 
-	return ss[2]
+	return strings.TrimSpace(ss[2])
+}
+
+func CreateHTML() []byte {
+	html := make([]string, 0, 1024)
+	html = append(html, "<html><body>")
+	html = append(html, "<h3>Swarm net test:</h3>")
+	html = append(html, fmt.Sprintf("<p>Current container time: %v<br>", time.Now()))
+	html = append(html, "<i>(if not changed after reload, then this page is cached)</i></p>")
+	html = append(html, fmt.Sprintf("<p>Container id: <b>%v</b><br>", DockerId()))
+	html = append(html, "<i>(this id is the same as in `docker ps`)</i></p>")
+	html = append(html, "</body></html>")
+	return []byte(strings.Join(html, ""))
+}
+
+func CreateJSON() []byte {
+	json := make([]string, 0, 1024)
+	json = append(json, "{")
+	json = append(json, fmt.Sprintf("\"timestamp\": \"%v\",", time.Now()))
+	json = append(json, fmt.Sprintf("\"container_id\": \"%v\"", DockerId()))
+	json = append(json, "}")
+	return []byte(strings.Join(json, ""))
 }
